33be22f44b92e99abfc42e26d2b7ecb3da6b62af
susmithanunna <126801279+susmithanunna@users.noreply.github.com>
2023-10-31 19:37:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rotation/rotation.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().findKRotation(a, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int findKRotation(int arr[], int n) {
+        // code here
+        int s=0;
+        int e=n-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]>=arr[s] && arr[mid]>=arr[e]){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+        }
+        return s;
+    }
+}
