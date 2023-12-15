bfcc26242cb0e98d24d62137f73eceec125a6f28
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2023-12-15 19:12:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reach the Nth point/reach-the-nth-point.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.nthPoint(n);
+            System.out.println(ans);            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int nthPoint(int n)
+    {
+        // Code here
+        int a=1;
+        int b=1;
+        int c=0;
+        for(int i=1;i<n;i++){
+            c=(a+b)%1000000007;
+            a=b%1000000007;
+            b=c%1000000007;
+        }
+        return b;
+    }
+}
