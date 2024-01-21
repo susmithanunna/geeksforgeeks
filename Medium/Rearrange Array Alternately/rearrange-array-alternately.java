ddb410c451bec2cdb4d590f541890cfdf3bbd490
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-01-21 10:57:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rearrange Array Alternately/rearrange-array-alternately.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        // Your code here
+       if(n==1){
+           
+       }
+       else{
+           int i=0;
+           int j=n-1;
+           ArrayList<Long> a1=new ArrayList<>();
+           while(i!=j && i<j){
+               a1.add(arr[j]);
+               a1.add(arr[i]);
+               j--;
+               i++;
+           }
+           if(i==j){
+               a1.add(arr[j]);
+           }
+           for(int k=0;k<a1.size();k++){
+               arr[k]=a1.get(k);
+           }
+       }
+        
+    }
+    
+}
+
+
