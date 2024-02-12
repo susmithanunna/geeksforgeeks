c98fa80263e5d7dbb63f1a2cfc4828760fb42200
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-02-12 21:15:35 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reach the Nth point/reach-the-nth-point.java
+++ Easy/Reach the Nth point/reach-the-nth-point.java
@@ -30,14 +30,33 @@ class Solution
     public int nthPoint(int n)
     {
         // Code here
-        int a=1;
-        int b=1;
-        int c=0;
-        for(int i=1;i<n;i++){
-            c=(a+b)%1000000007;
-            a=b%1000000007;
-            b=c%1000000007;
+        // int a=1;
+        // int b=1;
+        // int c=0;
+        // for(int i=1;i<n;i++){
+        //     c=(a+b)%1000000007;
+        //     a=b%1000000007;
+        //     b=c%1000000007;
+        // }
+        // return b;
+        // int mod=(int)Math.pow(10,9)+7;
+        // if(n==1){
+        //     return 1;
+        // }
+        // else if(n==2){
+        //     return 2;
+        // }
+        // return (nthPoint(n-1)+nthPoint(n-2))%mod;
+        int mod=(int)Math.pow(10,9)+7;
+        int dp[]=new int[n];
+        if(n==1){
+            return 1;
         }
-        return b;
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%mod;
+        }
+        return dp[n-1];
     }
 }
