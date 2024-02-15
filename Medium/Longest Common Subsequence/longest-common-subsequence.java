af7edd0aa205170780138d2b9c884d4adf078927
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-02-15 21:06:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.java
+++ Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -31,20 +31,23 @@ class Solution
     {
         // your code here
         int dp[][]=new int[x+1][y+1];
-        for(int i[]:dp){
-            Arrays.fill(i,0);
+        for(int i=0;i<=x;i++){
+            dp[i][0]=0;
         }
-        for(int i=x-1;i>=0;i--){
-            for(int j=y-1;j>=0;j--){
-                if(s1.charAt(i)==s2.charAt(j)){
-                    dp[i][j]=1+dp[i+1][j+1];
+        for(int i=0;i<=y;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=y;j++){
+                if(s1.charAt(i-1)!=s2.charAt(j-1)){
+                    dp[i][j]=(int)Math.max(dp[i][j-1],dp[i-1][j]);
                 }
                 else{
-                    dp[i][j]=(int)Math.max(dp[i][j+1],dp[i+1][j]);
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
             }
         }
-        return dp[0][0];
+       return dp[x][y];
     }
     
 }
