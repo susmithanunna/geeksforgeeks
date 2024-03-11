b7980f790c9fbe463ab8a0c30fa07c19a3c6c20d
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-03-11 19:47:16 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -48,20 +48,48 @@ class GFG {
 class Solution {
     int countPairs(int mat1[][], int mat2[][], int n, int x) {
         // code here
-        HashSet<Integer> s1=new HashSet<>();
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                s1.add(mat2[i][j]);
+        // HashSet<Integer> s1=new HashSet<>();
+        // int c=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         s1.add(mat2[i][j]);
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(s1.contains(x-mat1[i][j])){
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
+        int ans=0;
+        int i=0;
+        int j=n-1;
+        int c1=0;
+        int c2=n-1;
+        while(i<n && j>=0){
+            int val=mat1[i][c1]+mat2[j][c2];
+            if(val==x){
+                ans++;
+                c1++;
+                c2--;
+            }
+            else if(val<x){
+                c1++;
+            }
+            else{
+                c2--;
+            }
+            if(c1==n){
+                c1=0;
+                i++;
+            }
+            if(c2==-1){
+                c2=n-1;
+                j--;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(s1.contains(x-mat1[i][j])){
-                    c++;
-                }
-            }
-        }
-        return c;
+        return ans;
     }
 }
