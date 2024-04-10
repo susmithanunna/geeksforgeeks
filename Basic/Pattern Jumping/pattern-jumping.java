ae76855beaa997256ec95775c6f0ba8bbc3c1c7a
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-04-10 22:00:30 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pattern Jumping/pattern-jumping.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.canJump(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    static String canJump(Long N) {
+        // code here
+        if(N==1){
+            return "True";
+        }
+        if((N & (N-1))==0){
+            return "True";
+        }
+        return "False";
+    }
+};
