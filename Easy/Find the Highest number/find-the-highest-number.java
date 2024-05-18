01b0e2ef403787fb952444bd77ede6d834aba94c
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-05-18 09:59:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the Highest number/find-the-highest-number.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s = br.readLine().trim();
+            int n = Integer.parseInt(s);
+            String S = br.readLine();
+            String[] s1 = S.split(" ");
+            List<Integer> a = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++) {
+                a.add(Integer.parseInt(s1[i]));
+            }
+            Solution ob = new Solution();
+            int ans = ob.findPeakElement(a);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int findPeakElement(List<Integer> a) {
+        // Code here
+        int i=0;
+        int j=a.size()-1;
+        int max=Integer.MIN_VALUE;
+        while(i<=j){
+            if(max<a.get(i)){
+                max=a.get(i);
+                
+            }
+            if(max<a.get(j)){
+                max=a.get(j);
+            }
+            i++;
+            j--;
+        }
+        return max;
+    }
+}
