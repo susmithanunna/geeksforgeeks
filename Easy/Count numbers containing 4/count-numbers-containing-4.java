033a21f7fd782459ccb638211c4968c97ae24309
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-06-12 09:45:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count numbers containing 4/count-numbers-containing-4.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            int res = obj.countNumberswith4(n);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int countNumberswith4(int n) {
+        // code here
+        String t="4";
+        int c=0;
+        for(int i=1;i<=n;i++){
+            String s=String.valueOf(i);
+            if(s.indexOf(t)!=-1){
+                c++;
+            }
+        }
+        return c;
+    }
+}
