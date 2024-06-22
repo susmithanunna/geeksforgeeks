80d889765c57a458dfd51ac92f016ca6ba77416e
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-06-22 10:25:55 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if(ob.isSubsetSum(N, arr, sum))
+            System.out.println(1);
+            else
+            System.out.println(0);
+
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+
+
+    static Boolean isSubsetSum(int N, int arr[], int sum){
+        // code here
+        // boolean dp[][]=new boolean[N][sum+1];
+        // for(int i=0;i<N;i++){
+        //     dp[i][0]=true;
+        // }
+        // if(arr[0]<=sum){
+        //     dp[0][arr[0]]=true;
+        // }
+        // for(int i=1;i<N;i++){
+        //     for(int target=1;target<=sum;target++){
+        //         boolean nottake=dp[i-1][target];
+        //         boolean take=false;
+        //         if(arr[i]<=target){
+        //             take=dp[i-1][target-arr[i]];
+        //         }
+        //         dp[i][target]=take||nottake;
+        //     }
+        // }
+        // return dp[N-1][sum];
+        Boolean dp[][]=new Boolean[N+1][sum+1];
+        for(int i=0;i<=N;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0 ){
+                    dp[i][j]=false;
+                }
+                if(j==0){
+                    dp[i][j]=true;
+                }
+                if(i!=0 && arr[i-1]<=j){
+                    dp[i][j]=(dp[i-1][j-arr[i-1]] || dp[i-1][j]);
+                }
+                else if(i!=0){
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[N][sum];
+    }
+}
