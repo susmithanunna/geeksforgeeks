737c434958f470711b5d2a1f6aca317138e56099
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-07-27 09:26:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.java
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.java
@@ -1,54 +1,47 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
 import java.io.*;
-import java.util.*;
+import java.util.*; 
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG{
+    public static void main(String args[]) throws IOException { 
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            
-            String S = read.readLine().trim();
+        
+        while(t-- > 0){
+            String str = read.readLine();
+
             Solution ob = new Solution();
-            System.out.println(ob.findMinInsertions(S));
+            
+            System.out.println(ob.countMin(str));
         }
-    }
-}
+    } 
+} 
 // } Driver Code Ends
 
 
 //User function Template for Java
 
 class Solution{
-    int findMinInsertions(String s){
+    static int countMin(String str)
+    {
         // code here
-        int n=s.length();
+        int n=str.length();
         int dp[][]=new int[n+1][n+1];
-        StringBuilder s1=new StringBuilder(s);
-       String s2= s1.reverse().toString();
+        String rev=new StringBuilder(str).reverse().toString();
         for(int i=0;i<=n;i++){
-           // int k=n-1;
             for(int j=0;j<=n;j++){
                 if(i==0 || j==0){
                     dp[i][j]=0;
                 }
-                else {
-                    if(s.charAt(i-1)==s2.charAt(j-1)){
-                        dp[i][j]=dp[i-1][j-1]+1;
-                        //k--;
-                    }
-                    else{
-                        dp[i][j]=(int)Math.max(dp[i-1][j],dp[i][j-1]);
-                    }
-                    //k--;
+                else if(str.charAt(i-1)==rev.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=(int)Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+        // System.out.println(dp[n][n]);
         return n-dp[n][n];
     }
 }
