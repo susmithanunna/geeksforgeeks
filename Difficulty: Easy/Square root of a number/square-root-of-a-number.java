3db6a56d772aff21b114af8880cc32fb89b0dd2d
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-10-13 17:49:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square root of a number/square-root-of-a-number.java
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.java
@@ -24,6 +24,24 @@ this function*/
 class Solution {
     long floorSqrt(long n) {
         // Your code here
-        return (long)Math.sqrt(n);
+        long low=0;
+        long high=n;
+        long ans=-1;
+        while(low<=high){
+            long mid=(low+high)/2;
+            //System.out.println(mid);
+            if(mid*mid==n){
+                
+                return mid;
+            }
+            else if(mid*mid<n){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
     }
 }
