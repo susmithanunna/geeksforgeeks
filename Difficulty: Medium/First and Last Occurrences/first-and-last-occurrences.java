334c117540030450107dfdf1806a4a22de0ba7ae
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-11-08 14:37:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class GFG {
+    ArrayList<Integer> find(int arr[], int x) {
+        // code here
+        int f=-1;
+        int l=-1;
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                f=mid;
+                high=mid-1;
+            }
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        low=0;
+        high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                l=mid;
+                low=mid+1;
+            }
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        ArrayList<Integer> ans=new ArrayList<>();
+        ans.add(f);
+        ans.add(l);
+        return ans;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int n = a1.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(a1[i]);
+            }
+            int x = Integer.parseInt(br.readLine());
+            GFG ob = new GFG();
+            ArrayList<Integer> ans = ob.find(arr, x);
+            System.out.println(ans.get(0) + " " + ans.get(1));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
