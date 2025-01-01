d204278fa3a0551763b216eac8aff4115fe538b3
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2025-01-01 21:01:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -47,17 +47,14 @@ class Solution {
         int n=arr.length;
         int sum=0;
         int max=Integer.MIN_VALUE;
-       
         for(int i=0;i<n;i++){
             sum=sum+arr[i];
-             max=(int)Math.max(sum,max);
+            max=(int)Math.max(sum,max);
             if(sum<0){
                 sum=0;
-              
             }
-          
+            
         }
-        
         return max;
     }
 }
