bdbb957da6a13db082be95702f0b8be32ef48ec4
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2025-01-06 19:26:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Three way partitioning/three-way-partitioning.java
+++ Difficulty: Easy/Three way partitioning/three-way-partitioning.java
@@ -1,108 +1,125 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
+// Initial Template for Java
 import java.io.*;
+import java.lang.*;
 import java.util.*;
 
-class GFG{
-    public static void main(String[] args)throws IOException {
-        BufferedReader read= new BufferedReader(new InputStreamReader(System.in));
-        int t  =Integer.parseInt(read.readLine());
-         
-        while(t-->0)
-        {
-            int N = Integer.parseInt(read.readLine());
-            int array[] = new int[N];
-            Map<Integer, Integer> mp = new HashMap<>();
-            
-            String input_line[] = read.readLine().trim().split(" ");
-            for(int i=0;i<N;i++)
-            {
-                array[i] = Integer.parseInt(input_line[i]);
-                if (mp.containsKey(array[i]))  
-                { 
-                    mp.put(array[i], mp.get(array[i]) + 1); 
-                }  
-                else
-                { 
-                    mp.put(array[i], 1); 
-                } 
-            }
-            
-            input_line = read.readLine().trim().split(" ");
-            int a = Integer.parseInt(input_line[0]);
-            int b = Integer.parseInt(input_line[1]);
-        
-            int original[] = new int[N];
-            
-            for(int i=0; i<N;i++)
-            {
-                original[i] = array[i];
-            }
 
-            int k1=0,k2=0,k3=0;
-            int kk1=0;int kk2=0;int kk3=0;
+// } Driver Code Ends
+// User function Template for Java
 
-            for(int i=0;i<N;i++)
-            {
-                if(original[i]>b)
-                    k3++;
-                else if(original[i]<=b && original[i]>=a)
-                    k2++;
-                else if(original[i]<b)
-                    k1++;
+class Solution {
+    // Function to partition the array around the range such
+    // that array is divided into three parts.
+    public void threeWayPartition(int arr[], int a, int b) {
+        // code here
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        int ind=0;;
+        while(ind<n){
+            if(j<ind){
+                break;
             }
-            Solution ob = new Solution();
-            ob.threeWayPartition(array,a,b);
-
-            for(int i=0;i<k1;i++)
-            {
-                if(array[i]<b)
-                kk1++;
-            }
-            
-            for(int i=k1;i<k1+k2;i++)
-            {
-                if(array[i]<=b && array[i]>=a)
-                kk2++;
+            if(arr[ind]<a){
+                int t=arr[ind];
+                arr[ind]=arr[i];
+                arr[i]=t;
+                i++;
+                ind++;
                 
             }
-            
-            for(int i=k1+k2;i<k1+k2+k3;i++)
-            {
-                if(array[i]>b)
-                kk3++;
+            else if(arr[ind]>b){
+                int t=arr[ind];
+                arr[ind]=arr[j];
+                arr[j]=t;
+                j--;
+               
             }
-            Boolean ok = false;
-            if(k1==kk1 && k2 ==kk2 && k3 == kk3)
-                ok = true;
+            else{
+                ind++;
+            }
+        }
+        
+        
+    }
+}
 
-            for(int i=0;i<array.length;i++)
-                mp.put(array[i], mp.get(array[i]) - 1);
-            
-            for(int i=0;i<array.length;i++)
-                if(mp.get(array[i])!=0)
-                    ok=false;
-            
-            if(ok)
-                System.out.println(1);
-            else
-                System.out.println(0);
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int a = Integer.parseInt(br.readLine());
+            int b = Integer.parseInt(br.readLine());
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            obj.threeWayPartition(arr, a, b);
+            int ia = 0;
+            int ib = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= a) {
+                    ia = i;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > b) {
+                    ib = i;
+                    break;
+                }
+            }
+
+            int f = 0;
+
+            for (int i = 0; i < ia; i++) {
+                if (arr[i] >= a) {
+                    f = 1;
+                    break;
+                }
+            }
+
+            for (int i = ia; i < ib; i++) {
+                if (arr[i] > b || arr[i] < a) {
+                    f = 1;
+                    break;
+                }
+            }
+
+            for (int i = ib; i < arr.length; i++) {
+                if (arr[i] < b) {
+                    f = 1;
+                    break;
+                }
+            }
+
+            if (f == 1) {
+                System.out.println("false");
+            } else {
+                System.out.println("true");
+            }
+
+            System.out.println("~");
         }
     }
 }
 
 // } Driver Code Ends
-
-
-//User function Template for Java
-
-class Solution{
-    //Function to partition the array around the range such 
-    //that array is divided into three parts.
-    public void threeWayPartition(int array[], int a, int b)
-    {
-        // code here 
-        Arrays.sort(array);
-    }
-}
