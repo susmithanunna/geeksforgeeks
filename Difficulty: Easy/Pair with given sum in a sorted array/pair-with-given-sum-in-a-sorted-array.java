8f3138f532028964b7c31931313b4b3f5a190dd1
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2025-01-07 20:48:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
@@ -13,83 +13,83 @@ class Solution {
 
     int countPairs(int arr[], int target) {
         // Complete the function
-    //     int n=arr.length;
-    //   int i=0;
-    //   int j=n-1;
-    //   int ans=0;
-    //      while(i<j){
-    //          if(arr[i]+arr[j]==target){
-    //              //System.out.println(arr[i]+" "+arr[j]+" "+i+" "+j);
+        int n=arr.length;
+      int i=0;
+      int j=n-1;
+      int ans=0;
+         while(i<j){
+             if(arr[i]+arr[j]==target){
+                 //System.out.println(arr[i]+" "+arr[j]+" "+i+" "+j);
                 
-    //              if(arr[i]==arr[j]){
-    //                  int t=j-i;
-    //                      ans=ans+(t*(t+1))/2;
-    //                      break;
-    //              }
-    //              else{
-    //                  ans++;
-    //                  while(i+1<j && arr[i+1]+arr[j]==target){
-    //                   i++;
-    //                   ans++;
-    //                  }
-    //                  while(j-1>i && arr[j-1]+arr[i]==target){
-    //                      j--;
-    //                      ans++;
-    //                  }
-    //              }
-                 
-    //              j--;
-    //          }
-    //          else if(arr[i]+arr[j]>target){
-    //              j--;
-    //          }
-    //          else{
-    //              i++;
-    //          }
-    //      }
-    //      return ans;
-     int left = 0, right = arr.length - 1;
-        int count = 0;
+                 if(arr[i]==arr[j]){
+                     int t=j-i;
+                         ans=ans+(t*(t+1))/2;
+                         break;
+                 }
+                 else{
+                     int leftcount=1;
+                     int rightcount=1;
+                     while(i+1<j && arr[i+1]==arr[i]){
+                      i++;
+                      leftcount++;
+                     }
+                     while(j-1>i && arr[j-1]==arr[j]){
+                         j--;
+                         rightcount++;
+                     }
+                     ans=ans+leftcount*rightcount;
+                 }
+                 i++;
+                 j--;
+             }
+             else if(arr[i]+arr[j]>target){
+                 j--;
+             }
+             else{
+                 i++;
+             }
+         }
+         return ans;
+    //  int left = 0, right = arr.length - 1;
+    //     int count = 0;
 
-        while (left < right) {
-            int currentSum = arr[left] + arr[right];
+    //     while (left < right) {
+    //         int currentSum = arr[left] + arr[right];
 
-            if (currentSum == target) {
-                if(arr[left]==arr[right]){
-                    int t=right-left;
-                    count=count+(t*(t+1))/2;
-                    break;
-                }
+    //         if (currentSum == target) {
+    //             if(arr[left]==arr[right]){
+    //                 int t=right-left;
+    //                 count=count+(t*(t+1))/2;
+    //                 break;
+    //             }
                 
-                int leftVal = arr[left];
-                int rightVal = arr[right];
-                int leftCount = 1, rightCount = 1;
+    //             int leftVal = arr[left];
+    //             int rightVal = arr[right];
+    //             int leftCount = 1, rightCount = 1;
 
-                while (left + 1 < right && arr[left] == arr[left + 1]) {
-                    leftCount++;
-                    left++;
-                }
+    //             while (left + 1 < right && arr[left] == arr[left + 1]) {
+    //                 leftCount++;
+    //                 left++;
+    //             }
+    //             while (right - 1 > left && arr[right] == arr[right - 1]) {
+    //                 rightCount++;
+    //                 right--;
+    //             }
 
-                // Count duplicates for the right value
-                while (right - 1 > left && arr[right] == arr[right - 1]) {
-                    rightCount++;
-                    right--;
-                }
+    //             // Add all combinations of these duplicates
+    //             count += leftCount * rightCount;
 
-                // Add all combinations of these duplicates
-                count += leftCount * rightCount;
+    //             // Move past the current values
+    //             left++;
+    //             right--;
+    //         } else if (currentSum < target) {
+    //             left++;
+    //         } else {
+    //             right--;
+    //         }
+    //     }
 
-                // Move past the current values
-                left++;
-                right--;
-            } else if (currentSum < target) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        return count;
+    //     return count;
     }
 }
 
