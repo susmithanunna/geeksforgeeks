ab8e0623ca000bae64749411c15526ee12b12ac2
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2025-01-26 18:58:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.java
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.java
@@ -19,7 +19,9 @@ class GFG {
             Solution ob = new Solution();
             int res = ob.findKRotation(arr);
             System.out.println(res);
-        }
+        
+System.out.println("~");
+}
     }
 }
 
@@ -32,36 +34,27 @@ class Solution {
     public int findKRotation(List<Integer> arr) {
         // Code here
         int n=arr.size();
-		int low=0;
-		int high=n-1;
-		int min=Integer.MAX_VALUE;
-		int index=-1;
-		while(low<=high){
-		    int mid=(low+high)/2;
-		    if(arr.get(low)<=arr.get(high)){
-		        if(min>=arr.get(low)){
-		            return low;
-		        }
-		    }
-		    if(arr.get(low)<=arr.get(mid)){
-		        if(min>=arr.get(low)){
-		            min=arr.get(low);
-		            index=low;
-		            
-		        }
-		        low=mid+1;
-		    }
-		    else {
-		        if(min>=arr.get(mid)){
-		            min=arr.get(mid);
-		            index=mid;
-		        }
-		        high=mid-1;
-		    }
-		}
-		if(min>arr.get(low)){
-		    index=low;
-		}
-		return index;
+        int low=0;
+        int high=n-1;
+        int minindex=-1;
+        int min=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr.get(low)<=arr.get(mid)){
+                if(min>=arr.get(low)){
+                    minindex=low;
+                    min=arr.get(low);
+                }
+                low=mid+1;
+            }
+            else{
+                 if(min>=arr.get(mid)){
+                    minindex=mid;
+                    min=arr.get(mid);
+                }
+                high=mid-1;
+            }
+        }
+        return minindex;
     }
 }
