9cf145106bb71f409f0a2ddc8231dabbad99b70d
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2025-02-19 19:02:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    /* Function to print nodes in a given linked list */
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    // Driver program to test the above functions
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(scanner.nextLine());
+            List<Node> lists = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                String line = scanner.nextLine();
+                String[] values = line.split(" ");
+                Node head = null, temp = null;
+
+                for (String value : values) {
+                    Node newNode = new Node(Integer.parseInt(value));
+                    if (head == null) {
+                        head = newNode;
+                        temp = head;
+                    } else {
+                        temp.next = newNode;
+                        temp = temp.next;
+                    }
+                }
+
+                lists.add(head);
+            }
+
+            Solution sol = new Solution();
+            Node head = sol.mergeKLists(lists);
+
+            printList(head);
+            System.out.println("~");
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*class Node
+{
+    int data;
+    Node next;
+
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// arr is an array of Nodes of the heads of linked lists
+
+class Solution {
+    // Function to merge K sorted linked list.
+    Node mergeKLists(List<Node> arr) {
+        // Add your code here.
+    Node ans=null;
+    for(Node node:arr){
+        ans=merged(ans,node);
+    }
+    return ans;
+       
+    }
+    public Node merged(Node node1,Node node2){
+        Node merge=new Node(-1);
+        Node dummy=merge;
+        while(node1!=null && node2!=null){
+            if(node1.data<=node2.data){
+                dummy.next=node1;
+                node1=node1.next;
+                dummy=dummy.next;
+            }
+            else{
+                dummy.next=node2;
+                node2=node2.next;
+                dummy=dummy.next;
+            }
+        }
+        if(node1!=null){
+            dummy.next=node1;
+        }
+        if(node2!=null){
+            dummy.next=node2;
+        }
+        return merge.next;
+    }
+}
