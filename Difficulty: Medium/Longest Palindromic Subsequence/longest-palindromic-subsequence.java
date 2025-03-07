797452b136dd0e6e44340948538f5cefaa9c0e65
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2025-03-07 19:10:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.java
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.java
@@ -1,51 +1,45 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GfG
-{
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    String s = sc.next();
-                    Solution obj = new Solution();
-                    System.out.println(obj.longestPalinSubseq(s));
-                }
-                
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.longestPalinSubseq(s));
+
+            System.out.println("~");
         }
+    }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution
-{
-    public int longestPalinSubseq(String S)
-    {
-        //code here
-        int n=S.length();
-        StringBuilder s1=new StringBuilder(S).reverse();
-        String r=s1.toString();
+class Solution {
+    public int longestPalinSubseq(String s) {
+        // code here
+        int n=s.length();
         int dp[][]=new int[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
-                else if(S.charAt(i-1)==r.charAt(j-1)){
+        String rev=new StringBuilder(s).reverse().toString();
+        int max=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==rev.charAt(j-1)){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
                     dp[i][j]=(int)Math.max(dp[i-1][j],dp[i][j-1]);
                 }
+                max=(int)Math.max(dp[i][j],max);
             }
         }
-        return dp[n][n];
+        return max;
     }
 }
